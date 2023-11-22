a1411bda94de0378ad86d37d86b5966cfa11b38a
thibault <thibault.henrion@gmail.com>
2023-11-22 16:06:06 +0100

    add test mail sender V2

--- java/src/test/java/info/dmerej/MailSenderTest.java
+++ java/src/test/java/info/dmerej/MailSenderTest.java
@@ -18,11 +18,21 @@ public class MailSenderTest {
          new MailSender(http).sendV1(user, message);
 
          //test
-        Assert.assertTrue(http.isSent());
+        Assert.assertEquals(1, http.getSent());
     }
 
     @Test
     public void should_retry_when_getting_a_503_error() {
         // TODO: write a test to demonstrate the bug in MailSender.sendV2()
+        User user = new User("karlos", "karols@gmail.com");
+        String message = "message";
+        SendMailRequest request = new SendMailRequest(user.email(),"New notification", message);
+        MockHttpClient http = new MockHttpClient(request);
+
+        //treatment
+        new MailSender(http).sendV2(user, message);
+
+        //test
+        Assert.assertEquals(2, http.getSent());
     }
 }
--- java/src/test/java/info/dmerej/MockHttpClient.java
+++ java/src/test/java/info/dmerej/MockHttpClient.java
@@ -4,18 +4,20 @@ import info.dmerej.mailprovider.SendMailRequest;
 import info.dmerej.mailprovider.SendMailResponse;
 import lombok.Getter;
 
+
+
 public class MockHttpClient implements  HttpClient{
     private final SendMailRequest excpectedRequest ;
 
     @Getter
-    private boolean sent = false;
+    private int sent = 0;
     public MockHttpClient(SendMailRequest request){
         this.excpectedRequest = request;
     }
     public SendMailResponse post(String url, Object request){
-        if (excpectedRequest.equals(request)){
-            sent = true;
-            return new SendMailResponse(200, "message");
+        if (request instanceof SendMailRequest && excpectedRequest.equals(request)){
+            sent++;
+            return new SendMailResponse(503, "message");
         }
         return new SendMailResponse(503, "Error");
     }
